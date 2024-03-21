68e74ddbbadd4ed66404adefa5c919edfa3485cd
ANelson44 <145131926+ANelson44@users.noreply.github.com>
2024-03-20 21:35:59 -0400

    updated secret to fetch from .env

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,8 @@
 const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-const secret = '...';
+const secret = process.env.SECRET_KEY;
 
 
 module.exports = {
